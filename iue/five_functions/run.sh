ad662c91aed8f265d4348af41154e148f6aabf19
Kevin Albertson <kevin.albertson@mongodb.com>
2024-01-11 11:29:27 -0500

    update five_functions dependency

--- iue/five_functions/run.sh
+++ iue/five_functions/run.sh
@@ -1,5 +1,5 @@
 export MONGOCRYPT_LIB_DIR=/Users/kevin.albertson/code/c-bootstrap/install/libmongocrypt-1.8.0/lib
 export DYLD_LIBRARY_PATH=$MONGOCRYPT_LIB_DIR
 export PATH=$PATH:/Users/q/bin/mongodb-macos-aarch64-enterprise-6.0.8/bin
-# export KMS_PROVIDERS_PATH=~/.iue/kms_providers.json
+export KMS_PROVIDERS_PATH=~/.csfle/kms_providers.json
 cargo run
