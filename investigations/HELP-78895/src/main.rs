bcf5bb1014e484f9fd1fdacbfe56be3b9c0d51a8
Kevin Albertson <kevin.albertson@mongodb.com>
2025-10-28 13:00:15 -0400

    remove unused crate

--- investigations/HELP-78895/src/main.rs
+++ investigations/HELP-78895/src/main.rs
@@ -2,14 +2,13 @@ use tokio;
 
 use mongodb::{
     bson::doc,
-    options::{ClientOptions, ResolverConfig},
+    options::ClientOptions,
     Client,
 };
 
 #[tokio::main]
 async fn main() -> mongodb::error::Result<()> {
     let uri = std::env::var("MONGODB_URI").expect("MONGODB_URI environment variable must be set");
-    // let opts = ClientOptions::parse(uri).resolver_config(ResolverConfig::cloudflare()).await?;
     let opts = ClientOptions::parse(uri).await?;
     let client = Client::with_options(opts)?;
     let res = client
