7338653be2b985c43aac298369ac513be9107eba
Kevin Albertson <kevin.albertson@mongodb.com>
2025-11-10 08:43:03 -0500

    add env var to choose resolver

--- investigations/HELP-78895/src/main.rs
+++ investigations/HELP-78895/src/main.rs
@@ -9,9 +9,10 @@ use mongodb::{
 #[tokio::main]
 async fn main() -> mongodb::error::Result<()> {
     let uri = std::env::var("MONGODB_URI").expect("MONGODB_URI environment variable must be set");
+    let resolver = std::env::var("MONGODB_RESOLVER").expect("MONGODB_RESOLVER environment variable must be set");
     println!("Testing with URI: {}", uri);
 
-    {
+    if resolver == "system" {
         print!("Testing system resolver ... ");
         let opts = ClientOptions::parse(uri.clone()).await?;
         let client = Client::with_options(opts)?;
@@ -26,7 +27,7 @@ async fn main() -> mongodb::error::Result<()> {
         }
     }
 
-    {
+    else if resolver == "cloudflare"{
         print!("Testing CloudFlare resolver ... ");
         let opts = ClientOptions::parse(uri.clone()).resolver_config(ResolverConfig::cloudflare()).await?;
         let client = Client::with_options(opts)?;
@@ -39,6 +40,10 @@ async fn main() -> mongodb::error::Result<()> {
         } else {
             println!("succeeded!");
         }
+    }
+
+    else {
+        panic!("Unknown resolver option: {}", resolver);
     }
 
     Ok(())
