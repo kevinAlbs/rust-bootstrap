36dd3595cd3a3c710ed6ccab22ef0a3ffe97609a
Kevin Albertson <kevin.albertson@mongodb.com>
2025-11-10 08:03:01 -0500

    test both CloudFlare and the system resolver

--- investigations/HELP-78895/src/main.rs
+++ investigations/HELP-78895/src/main.rs
@@ -2,20 +2,44 @@ use tokio;
 
 use mongodb::{
     bson::doc,
-    options::ClientOptions,
+    options::{ClientOptions, ResolverConfig},
     Client,
 };
 
 #[tokio::main]
 async fn main() -> mongodb::error::Result<()> {
     let uri = std::env::var("MONGODB_URI").expect("MONGODB_URI environment variable must be set");
-    let opts = ClientOptions::parse(uri).await?;
-    let client = Client::with_options(opts)?;
-    let res = client
-        .database("test")
-        .run_command(doc!{"ping": 1})
-        .await?;
-    println!("got: {:?}", res);
+    println!("Testing with URI: {}", uri);
+
+    {
+        print!("Testing default resolver ... ");
+        let opts = ClientOptions::parse(uri.clone()).await?;
+        let client = Client::with_options(opts)?;
+        let res = client
+            .database("test")
+            .run_command(doc!{"ping": 1})
+            .await;
+        if res.is_err() {
+            println!("failed: {:?}", res.err());
+        } else {
+            println!("succeeded!");
+        }
+    }
+
+    {
+        print!("Testing CloudFlare resolver ... ");
+        let opts = ClientOptions::parse(uri.clone()).resolver_config(ResolverConfig::cloudflare()).await?;
+        let client = Client::with_options(opts)?;
+        let res = client
+            .database("test")
+            .run_command(doc!{"ping": 1})
+            .await;
+        if res.is_err() {
+            println!("failed: {:?}", res.err());
+        } else {
+            println!("succeeded!");
+        }
+    }
 
     Ok(())
 }
