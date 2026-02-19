080e4a7f153abb1b13691727e08bd4a9df6fa67d
Kevin Albertson <kevin.albertson@mongodb.com>
2026-02-19 09:20:13 -0500

    simplify repro

--- investigations/tagset/src/main.rs
+++ investigations/tagset/src/main.rs
@@ -1,93 +1,54 @@
 use tokio;
 
 use mongodb::{
-    bson::doc,
-    event::{command::CommandEvent, EventHandler, sdam},
-    options::{ClientOptions,SelectionCriteria,TagSet,ReadPreference,ReadPreferenceOptions},
     Client,
+    bson::doc,
+    options::{ClientOptions, ReadPreference, ReadPreferenceOptions, SelectionCriteria, TagSet},
 };
-
-use std::sync::{Arc, Mutex};
-
-use std::collections::HashSet;
 
 #[tokio::main]
 async fn main() -> mongodb::error::Result<()> {
     let uri = "mongodb://localhost:27017/?replicaSet=repl0"; // Other nodes will be discovered by SDAM.
-    let mut opts = ClientOptions::parse(uri).await?;
+    let opts = ClientOptions::parse(uri).await?;
 
-    // Use a command started event to show which server was selected:
-    opts.command_event_handler = Some(EventHandler::callback(move |ev| {
-         match ev {
-            CommandEvent::Started(ev) => {
-                println!("Sending {} selected server {}", ev.command_name, ev.connection.address);
-            }
-            _ => (),
-        }
-    }));
-
-    // Track server changed events:
-    let counters = Arc::new(Mutex::new(HashSet::<String>::new()));
-    let counters_ref = counters.clone();
-    opts.sdam_event_handler = Some(EventHandler::callback(move |ev| {
-        match ev {
-            sdam::SdamEvent::ServerDescriptionChanged(ev) => {
-                println!("Server Description changed for: {} from {} to {}", ev.new_description.address(), ev.previous_description.server_type(), ev.new_description.server_type());
-                counters_ref.lock().unwrap().insert(ev.new_description.address().to_string());
-            }
-            _ => (),
-        }
-    }));
-    
     let client = Client::with_options(opts)?;
-    
-    // Wait for all servers to initially be discovered:
-    let reply = client.database("admin")
-        .run_command(doc! { "replSetGetStatus": 1 })
-        .await
-        .unwrap();
-    let node_count = reply.get_array("members").unwrap().len();
 
-    let mut servers_discovered = counters.lock().unwrap().len();
-    while servers_discovered < node_count {
-        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
-        servers_discovered = counters.lock().unwrap().len();
-    }
+    // Sleep to await initial discovery:
+    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
 
     // Use selection criteria described in https://mongodb.slack.com/archives/CP893U9MX/p1771031511000619
-    let selection_criteria = {
-        let analytics_tag_set: TagSet =
-            TagSet::from([("nodeType".to_string(), "ANALYTICS".to_string())]);
-
-        // An empty tag set is intended to select a secondary when there is no analytics node available.
-        let empty_tag_set = TagSet::new();
-
+    let selection_criteria =
         SelectionCriteria::ReadPreference(ReadPreference::SecondaryPreferred {
             options: Some(
                 ReadPreferenceOptions::builder()
-                    .tag_sets(Some(vec![empty_tag_set]))
+                    .tag_sets(Some(vec![TagSet::new()]))
                     .build(),
             ),
-        })
-    };
+        });
 
+    // Send replSetGetStatus. Expect to select secondary:
     let reply = client
         .database("admin")
-        .run_command(doc!{"replSetGetStatus": 1}).selection_criteria(selection_criteria)
+        .run_command(doc! {"replSetGetStatus": 1})
+        .selection_criteria(selection_criteria)
         .await?;
-
-    println!("Expecting to select server with _id:0");
     let mut found_self = false;
     for member in reply.get_array("members").unwrap().clone() {
         let member_doc = member.as_document().unwrap();
-        println!("{}", member_doc);
         if member_doc.get_bool("self").unwrap_or(false) {
             found_self = true;
-            assert_eq!(member_doc.get_i32("_id").unwrap(), 0);
+            let state_str = member_doc.get_str("stateStr").unwrap();
+            if state_str != "SECONDARY" {
+                println!(
+                    "ERROR: expected to select SECONDARY, but selected {}",
+                    state_str
+                );
+            } else {
+                println!("OK: selected SECONDARY");
+            }
         }
     }
     assert!(found_self);
 
     Ok(())
 }
-
--- investigations/tagset/test-pymongo.py
+++ investigations/tagset/test-pymongo.py
@@ -1,34 +1,24 @@
 from pymongo import MongoClient, read_preferences  
-from pymongo.monitoring import CommandListener, CommandStartedEvent
 from time import sleep
-  
-# Connection URI to your MongoDB cluster  
-uri = "mongodb://localhost:27017/?replicaSet=repl0"
-  
-# Define a custom CommandListener to monitor commands  
-class MyCommandListener(CommandListener):  
-    def started(self, event: CommandStartedEvent):  
-        # Print information about the server selected for each command  
-        print(f"Selected server: {event.connection_id}")  
-  
-    def succeeded(self, event):  
-        pass
-  
-    def failed(self, event):  
-        pass
-  
+
 # Create a MongoClient instance  
-client = MongoClient(uri, event_listeners=[MyCommandListener()])  
+client = MongoClient("mongodb://localhost:27017/?replicaSet=repl0")
   
-# Set up secondaryPreferred read preference with tag set  
-read_pref = read_preferences.SecondaryPreferred(tag_sets=[{"nodeType": "analytics"}, {}])
+# Set up secondaryPreferred read preference with an empty tag set
+read_pref = read_preferences.SecondaryPreferred(tag_sets=[{}])
 
 # Sleep to await initial discovery.
 sleep(1)
-  
-# Ping command using the preferred server
-try:  
-    client["admin"].command({"replSetGetStatus": 1}, read_preference=read_pref)
-    print("Ping succeeded")
-except Exception as e:  
-    print("Error:", e)  
+
+# Send replSetGetStatus. Expect to select secondary:
+reply = client["admin"].command({"replSetGetStatus": 1}, read_preference=read_pref)
+found_self = False
+for member in reply["members"]:
+    if "self" in member and member["self"]:
+        found_self = True
+        if member["stateStr"] != "SECONDARY":
+            print("ERROR: expected to select SECONDARY, but selected " + member["stateStr"])
+        else:
+            print("OK: selected SECONDARY")
+if not found_self:
+    print("ERROR: could not find self")
